4735686bc07a9b29c2d9c9a51b188bd565e559f2
kildong <jh_kildong@naver.com>
2024-03-12 12:41:20 +0900

    1

--- ProjectBS/Assets/_BsScripts/AnimEvent/AnimParam.cs
+++ ProjectBS/Assets/_BsScripts/AnimEvent/AnimParam.cs
@@ -7,4 +7,5 @@ public static class AnimParam
 {
     public static int Attack = Animator.StringToHash("Attack");
     public static int isAttacking = Animator.StringToHash("isAttacking");
+    public static int isMoving = Animator.StringToHash("isMoving");
 }
