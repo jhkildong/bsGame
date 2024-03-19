f2848dccca21478f1b054bbe6b509ff456776947
jeajun12 <wowns9179@naver.com>
2024-03-20 01:38:23 +0900

    Waepon Forwar, Ordital

---
+++ ProjectBS/Assets/_BsScripts/WeaponType/ForwardWeaponBullet.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForwardWeaponBullet : MonoBehaviour
+{
+    public float moveSpeed = 5.0f; // 이동 속도
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    }
+}
