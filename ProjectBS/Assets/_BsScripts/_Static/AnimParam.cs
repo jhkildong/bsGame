5aaa22ef57f9c22ec896a18e621e6030e82ccd05
kildong <jh_kildong@naver.com>
2024-04-17 01:16:54 +0900

    wizard 수정중

--- ProjectBS/Assets/_BsScripts/_Static/AnimParam.cs
+++ ProjectBS/Assets/_BsScripts/_Static/AnimParam.cs
@@ -7,10 +7,9 @@ public static class AnimParam
 {
     public static int Attack = Animator.StringToHash("Attack");
     public static int isAttacking = Animator.StringToHash("isAttacking");
-    public static int isComboReady = Animator.StringToHash("isComboReady");
     public static int isMoving = Animator.StringToHash("isMoving");
+    public static int isAttackMoving = Animator.StringToHash("isAttackMoving");
     public static int Death = Animator.StringToHash("Death");
-    public static int isDead = Animator.StringToHash("isDead");
     public static int x = Animator.StringToHash("x");
     public static int y = Animator.StringToHash("y");
 }
