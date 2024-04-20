2e686aa94021c5fda9de54794364b54a1f5702bc
kildong <jh_kildong@naver.com>
2024-04-21 02:05:20 +0900

    PlayerSelectUI추가, Wizard -> Mage변경

--- ProjectBS/Assets/_BsScripts/Player/Archer.cs
+++ ProjectBS/Assets/_BsScripts/Player/Archer.cs
@@ -2,38 +2,10 @@ using UnityEngine;
 
 public class Archer : PlayerComponent
 {
-    private enum EffectType
-    {
-        Lightning = 3510
-    }
-    const int defaultID = 3510;
-
-    public override Effect[] MyEffects
-    {
-        get
-        {
-            if (_effects.Length == 0)
-            {
-                _effects = Resources.LoadAll<Effect>("Effect/Archer");
-            }
-            return _effects;
-        }
-    }
-    [SerializeField] private EffectType _effectType;
-    public override Effect GetMyEffect()
-    {
-        return MyEffects[(int)_effectType - defaultID];
-    }
-
-    public void SelectMyEffect(int id)
-    {
-        _effectType = (EffectType)id;
-    }
-
     public override void OnAttackPoint()
     {
         //공격 이펙트 생성
-        ArrowEffect arrow = ObjectPoolManager.Instance.GetEffect(GetMyEffect(), Attack) as ArrowEffect;
+        ArrowEffect arrow = ObjectPoolManager.Instance.GetEffect(MyEffect, Attack) as ArrowEffect;
         arrow.This.transform.SetPositionAndRotation(MyEffectSpawn.position, MyEffectSpawn.rotation);
         arrow.Shoot();
     }
--- ProjectBS/Assets/_BsScripts/Player/PlayerComponent.cs
+++ ProjectBS/Assets/_BsScripts/Player/PlayerComponent.cs
@@ -1,10 +1,16 @@
-using System;
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
+
 
 //캐릭터 컴포넌트 관리해주는 클래스
 public abstract class PlayerComponent : CharacterComponent
 {
+    protected static class EffectID
+    {
+        public const int Warrior = 3500;
+        public const int Archer = 3510;
+        public const int Mage = 3520;
+    }
     #region Property
     ////////////////////////////////Property////////////////////////////////
     public Transform MyEffectSpawn 
@@ -35,20 +41,31 @@ public abstract class PlayerComponent : CharacterComponent
             return _rigs;
         }
     }
-    public abstract Effect[] MyEffects{ get; }
     public float Attack
     {
         get => _attack;
         set => _attack = value;
     }
+    public Effect MyEffect
+    { 
+        get => _effect;
+        set => _effect = value;
+    }
     #endregion
 
-    #region Private Field
+    #region Field
     ////////////////////////////////PrivateField////////////////////////////////
     [SerializeField] protected Transform _effectSpawn;
     [SerializeField] protected Rig[] _rigs;
-    [SerializeField] protected Effect[] _effects = new Effect[0];
     [SerializeField] protected float _attack;
+    [SerializeField] protected Effect[] _effects;
+    [SerializeField] protected int _effectIdx;
+    [SerializeField] protected Effect _effect;
+    #endregion
+
+    #region Abstract Method
+    ////////////////////////////////AbstractMethod////////////////////////////////
+    public abstract void OnAttackPoint();           //_effectSpawn에 공격 생성(애니메이션에서 호출)
     #endregion
 
     #region Public Method
@@ -60,8 +77,6 @@ public abstract class PlayerComponent : CharacterComponent
             rig.weight = weight;
         }
     }
-    public abstract Effect GetMyEffect();
 
-    public abstract void OnAttackPoint();
     #endregion
 }
