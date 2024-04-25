e7aace7e655be71bd8585b1242b7278888fa8734
fermat308 <115774319+fermat308@users.noreply.github.com>
2024-04-25 21:32:33 +0900

    계산식 추가, 건물에 수식적용

---
+++ ProjectBS/Assets/_BsScripts/Building/Buildings/AttackBuilding/TestRangeBuilding.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class TestRangeBuilding : AttackBuilding_Area
+{
+    public GameObject myEffect;
+
+    protected override void Start()
+    {
+        base.Start();
+        AtkEvent.AddListener(SetActiveEffects);
+    }
+
+    void SetActiveEffects()
+    {
+        if (target != null && atkDelaying)
+        {
+
+            EffectPoolManager.Instance.SetActiveRangeObject<PointAtkEffectHit>(myEffect, effectPool, target, _finalDmg, _atkRadius, 0, 0, _atkSpeed, _hitDelay, _atkDuration);
+
+        }
+    }
+}
