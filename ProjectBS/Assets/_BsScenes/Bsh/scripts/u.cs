b6e0e8955ffb1e719e63d40499c918444af0deb6
DanniBoi <67821504+Hello5123@users.noreply.github.com>
2024-05-05 20:56:32 +0900

    cursor+ dropitem
    cursor+ dropitem

--- ProjectBS/Assets/_BsScenes/Bsh/scripts/u.cs
+++ ProjectBS/Assets/_BsScenes/Bsh/scripts/u.cs
@@ -5,10 +5,12 @@ using TMPro;
 public class u : MonoBehaviour
 {
     public TMP_Text tMP_Text;
-    int Max_Hp, Cur_Hp,MoveSpeed, Attack, Mag, Gold;
+    int Max_Hp, Cur_Hp, MoveSpeed, Attack, Mag;
+    GameManager gameManager;
 
     void Start()
     {
-        tMP_Text.text = $"{Max_Hp}\n{Cur_Hp}\n{MoveSpeed}\n{Attack}\n{Mag}\n{Gold}";
+        gameManager = GameManager.Instance;
+        tMP_Text.text = $"{Max_Hp}\n{Cur_Hp}\n{MoveSpeed}\n{Attack}\n{Mag}\n{gameManager.CurGold()}\n{gameManager.CurLvExp}";
     }
 }
