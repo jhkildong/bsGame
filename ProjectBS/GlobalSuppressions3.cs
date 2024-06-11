07b65a681a535a1c89afffbbd096e661acd80ce9
kildong <75233528+jhkildong@users.noreply.github.com>
2024-06-12 03:04:55 +0900

    Delete ProjectBS/GlobalSuppressions3.cs

--- ProjectBS/GlobalSuppressions3.cs
+++
@@ -1,8 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Style", "IDE0044:읽기 전용 한정자 추가", Justification = "<보류 중>", Scope = "member", Target = "~F:BossMonsterDevil.impList")]
